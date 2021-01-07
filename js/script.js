7a54e4e030b476e15e6cef0005f01d3cc75d4896
Kiryjm <kiryjm@gmail.com>
2021-01-08 02:31:53 +0300

    Practice: personalMovieDB object and properties were created.

--- js/script.js
+++ js/script.js
@@ -1,73 +1,45 @@
 
 "use strict"; 
 
-//Expression interpolation
-//use template strings with back quotes  to embed variables into strings
-const category = 'toys';
-console.log(`https://someurl.com/${category}/`);
+/* Задание на урок:
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя 
+на вопрос: 'Сколько фильмов вы уже посмотрели?'
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+Проверить, чтобы все работало без ошибок в консоли */
 
-const user = "Lori";
+//1)
+const numberOfFilms = +prompt("How many movies have you already watch?", "");
 
-alert(`Hi, ${user}`);
+//2)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-//Operators in JS
-console.log('arr' + " - object");
+//3)
+const lastWatchedMovie1 = prompt("What is your one of the last watched movie?"),
+       movieRating1 = +prompt("How can you rate it?", ""),
+       lastWatchedMovie2 = prompt("What is your one of the last watched movie?"),
+       movieRating2 = +prompt("How can you rate it?", "");
 
-console.log(4 + +" - object"); //NAN
-console.log(4 + +"5"); //9
+personalMovieDB.movies[lastWatchedMovie1] = movieRating1;
+personalMovieDB.movies[lastWatchedMovie2] = movieRating2;
 
-//increment/decrement
-let incr = 10,
-    decr = 10;
-
-// incr++;
-// decr--;
-
-//postfix form
-console.log(incr++); //10
-console.log(incr); //11
-
-console.log(decr--); //10
-console.log(decr); //9
-
-//prefix form
-console.log(++incr); //10
-console.log(incr); //11
-
-console.log(--decr); //10
-console.log(decr); //9
-
-console.log(5%2); //1
-
-
-//non strict comparison(value)
-console.log(2 * 4 == '8'); //true
-
-// strict comparison(value and data type)
-console.log(2 * 4 === '8'); //false
-
-const isChecked = true,
-      isCLose = true;
-
-console.log(isChecked && isCLose);
-console.log(2 + 2 * 2 === 6); //true
-
-//Unary OR | and AND & operators for binary representation
-//used to form mask for adding/checking user permissions due to roles
-const ACCESS_ADMIN = 1;          // 00001
-const ACCESS_GOODS_EDIT = 2;   // 00010
-const ACCESS_GOODS_VIEW = 4;     // 00100
-const ACCESS_ARTICLE_EDIT = 8; // 01000
-const ACCESS_ARTICLE_VIEW = 16;  // 10000
-    
-const guest = ACCESS_ARTICLE_VIEW | ACCESS_GOODS_VIEW; // 10100
-const editor = guest | ACCESS_ARTICLE_EDIT | ACCESS_GOODS_EDIT; // 11110
-const admin = editor | ACCESS_ADMIN; // 11111
- 
-alert(editor & ACCESS_ADMIN); // 0, access denied
-alert(editor & ACCESS_ARTICLE_EDIT); // 8, access granted
-
-//Operator ~(binary not) used to round values
-alert( ~~12.345 ); // 12
-//XOR operator ^ used to round values
-alert( 12.345 ^ 0 ); // 12
+console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
