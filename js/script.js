d371aaea4f1028fca810bc4dd50b6433eadffb4a
Kiryjm <kiryjm@gmail.com>
2021-01-20 05:42:50 +0300

    Practice: functions for adding movies and genres were added.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,82 @@
+/* Задание на урок:
+1) Первую часть задания повторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+P.S. Функции вызывать не обязательно */
 
 "use strict"; 
 
-const str = "teSt";
-const arr = [1, 2, 4];
+let numberOfFilms;
 
-// console.log(str.length);
-// console.log(arr.length);
-console.log(str.toLowerCase());
-console.log(str);
+function start () {
+    numberOfFilms = +prompt('How many movies have you already watch?', '');
 
-const fruit = "Some fruit";
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies have you already watch?', '');
 
-console.log(fruit.indexOf("q")); //-1 means not find
+    }
+}
 
-const logg = "Hello world";
+start();
 
-//cut piece of string from/till position not including ends
-console.log(logg.slice(6, 11)); //world
+function detectPersonalLevel () {
+    if (numberOfFilms > 0 && numberOfFilms < 10) {
+        console.log("Quite a few movies were watched");
+    } else if (numberOfFilms >= 10 && numberOfFilms <=30) {
+        console.log("You are a classical spectator");
+    } else if (numberOfFilms > 30) {
+        console.log("You are movie fan");
+    } else {
+        console.log("Error occured");
+    }
+}
 
-//you can point from/till position from the end
-console.log(logg.slice(-5, -1)); // worl
+detectPersonalLevel();
 
-//the same without negative positions
-console.log(logg.substring(6, 11)); //world
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-//cut piece of string from indicated position neede number of symbols not including ends
-console.log(logg.substr(6, 5)); //world
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        const lastWatchedMovie = prompt('What is your one of the last watched movie?', ''),
+              movieRating = prompt('How can you rate it?', '');
+        if (lastWatchedMovie != null && movieRating != null && lastWatchedMovie != '' && 
+        lastWatchedMovie != '' && lastWatchedMovie.length <= 50 ) {
+            personalMovieDB.movies[lastWatchedMovie] = movieRating;
+            console.log('Done');
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
 
-const num = 12.2;
-console.log(Math.round(num)); //12
+rememberMyFilms();
 
-const test = "12.2px";
-//transform argument to another notation: parsing argument to integer
-console.log(parseInt(test)); //12 and this will be integer number
-console.log(parseFloat(test)); //12.2 and this will be float number
+function showMyDB () {
+    if(!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres () {
+    for(let i = 1; i < 4; i++) {
+        personalMovieDB.genres[i-1] = prompt(`What is your favourite genre by number ${i}?`);
+    }
+}
+
+writeYourGenres();
+
+// let str = "travelling";
+// console.log(parseInt(str));
+// console.log(typeof(parseInt(str)));
