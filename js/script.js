86c377a6a49124b3d48eb89173686e3cbccb8769
Kiryjm <kiryjm@gmail.com>
2021-01-26 18:53:16 +0300

    Dynamic typing. Parsing to string, number, boolean.

--- js/script.js
+++ js/script.js
@@ -1,87 +1,57 @@
-/* Задание на урок:
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
 "use strict"; 
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        personalMovieDB.count = +prompt('How many movies have you already watch?', '');
-    
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('How many movies have you already watch?', '');
-    
-        }
-    },
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            const lastWatchedMovie = prompt('What is your one of the last watched movie?', ''),
-                  movieRating = prompt('How can you rate it?', '');
-            if (lastWatchedMovie != null && movieRating != null && lastWatchedMovie != '' && 
-            lastWatchedMovie != '' && lastWatchedMovie.length <= 50 ) {
-                personalMovieDB.movies[lastWatchedMovie] = movieRating;
-                console.log('Done');
-            } else {
-                console.log('Error');
-                i--;
-            }
-        }
-    },
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
-            console.log("Quite a few movies were watched");
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
-            console.log("You are a classical spectator");
-        } else if (personalMovieDB.count > 30) {
-            console.log("You are movie fan");
-        } else {
-            console.log("Error occured");
-        }
-    },
-    showMyDB: function(hidden) {
-        if(!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB: function() {
-        if(personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i < 4; i++) {
-            let genre = prompt(`What is your favourite genre by number ${i}?`);
-            while (genre == null || genre == '') {
-                genre = prompt(`What is your favourite genre by number ${i}?`);
-            }
-            personalMovieDB.genres[i-1] = genre;
-        }
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Favourite genre #${i + 1} - ${item}`);
-        });
-    }
-};
+// Dynamic typing
 
-// personalMovieDB.start();
+// 1) to string
+console.log(typeof(String(null))); 
+console.log(typeof(String(4))); // string
 
-// personalMovieDB.detectPersonalLevel();
+// concatenation
+console.log(typeof(5 + '')); // string
 
-// personalMovieDB.rememberMyFilms();
+const num = 5;
+console.log("https://vk.com/catalog/" + num); // https://vk.com/catalog/5
 
-// personalMovieDB.showMyDB(personalMovieDB.privat);
+const fontSize = 26 + 'px';
 
-// personalMovieDB.writeYourGenres();
+//2) to number
+console.log(typeof(Number('4'))); // number
+
+console.log(typeof(+'4')); // number
+
+console.log(typeof(parseInt("15px", 10))); // number
+
+//All you get from user is a string datatype: from prompt, tag input or textarea, etc.
+let answ = +prompt("Hello", "");
+
+// To boolean
+// False: 0, '', null, undefined, NaN 
+
+let switcher = null; // false
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1; // true
+
+if (switcher) {
+    console.log('Working...');
+}
+
+console.log(typeof(Boolean('4'))); // boolean
+
+console.log(typeof(!"4444")); // boolean
+
+let exp = "!4444";
+console.log(parseInt(exp, 10));
+console.log(`${exp} is ${typeof(exp)} with value: ${exp.toString()}`);
+
+console.log(typeof([] + [])); // string
+console.log(typeof([] + ! + [])); // string
+console.log([] + ! + []); // true
+console.log([] + !! + []); // false
+
+
+
