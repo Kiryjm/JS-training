0707913b48580b39c16799330ed74b7fda02e8c6
Kiryjm <kiryjm@gmail.com>
2021-01-30 01:58:11 +0300

    DOM elements navigation.

--- js/script.js
+++ js/script.js
@@ -1,58 +1,37 @@
 'use strict';
 
-// Event handlers
+// DOM elements navigation, data-attributes, for/of
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+// console.log(document.head);
 
-// btn.onclick = function() {
-//     alert('Click');
-// };
+// // get html tag content
+// console.log(document.documentElement);
 
-// // the last onclick function call will be executed!
-// btn.onclick = function() {
-//     alert('Second click');
-// };
+// // get all nodes inside parent node
+// // doesn't have analog for getting all elements
+console.log(document.body.childNodes);
 
-// // two consequtive fucntion call on mouse click will be executed
-// btn.addEventListener('click', () => {
-//     alert('Click');
-// });
-
-// btn.addEventListener('click', () => {
-//     alert('Second click');
-// });
-
-// we can pass event as argument in callback-function to get element data
-// you can get element by accessing target property
-// .type property allows to show type of event happened
-// let i = 0;
-const deleteElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);   
-    // }
-};
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-// you must add eventListener in loop for pseudo arrays!
-// also you can pass options as argument. e.g. execute cllback once on click
-
-btns.forEach(item => {
-    item.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
+// // get first and last child of body tag
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// // get first and last element except text node
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
 
 
-link.addEventListener('click', (e) => {
 
-    // cancel standard browser behaviour to go href
-    e.preventDefault();
-    console.log(e.target);
-});
+// // get parent of the parent node of element with id = "current"
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+
+
+// // data attributes
+// // html attribute writenn in square brackets
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
