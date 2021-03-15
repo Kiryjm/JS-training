acff14940fe342604d62a08681336ec7d1d091b9
Kiryjm <kiryjm@gmail.com>
2021-03-15 05:18:13 +0300

    setInterval vs setTimeout, Animation with setInterval, recursive setTimeout.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,52 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
-    
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
+// // setTimeout function calls passed function after passed 2nd argument - time in ms
+// const timerId = setTimeout(logger, 2000, 'Hello');
 
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
+// Animation in JS with setInterval
+// default value of delay for setInterval is 4ms
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
-
-    hideTabContent();
-    showTabContent();
-
-    tabsParent.addEventListener('click', (event) => {
-        let target = event.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);                
-                }
-            });
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
         }
-    });
-});
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000, 'Hello');
+//      timerId = setInterval(logger, 2000);
+// });
+
+
+// function logger () {
+//     if (i == 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+// Recursive setTimeout better than setInterval, cause setInterval doesn't consider 
+// the time function will execute inside it and will reexecute this function again after 
+// specified time amount. 
+// setTimeout calls function once after specified time interval
+// setInterval calls function each time after specified time interval
+
+// // Recursive setTimeout 
+// let id = setTimeout(function log() {
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
+
