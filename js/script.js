9131bfcf6c1653ec31f3f0d3acb8092145f4b244
Kiryjm <kiryjm@gmail.com>
2021-03-16 17:02:44 +0300

    Food project: making countdown timer.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -35,4 +37,62 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer
+
+    const deadline = '2021-05-11';
+
+    function getTimeRemaining(endtime) {
+        const timeDiff = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(timeDiff / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((timeDiff / (1000 * 60)) % 60),
+              seconds = Math.floor((timeDiff / 1000) % 60);
+
+        return {
+            'total': timeDiff,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+// if number of date part < 10 show 0 first
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        // to prevent old numbers in timer appearance causes layout blinking 
+        // we should execute updateClock manually to initialize clock!! 
+        updateClock();
+
+        function updateClock() {
+            const timeObj = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(timeObj.days);
+            hours.innerHTML = getZero(timeObj.hours);
+            minutes.innerHTML = getZero(timeObj.minutes);
+            seconds.innerHTML = getZero(timeObj.seconds);
+            
+            if (timeObj.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
 });
