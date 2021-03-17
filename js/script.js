ee7ec076423f14577c58238760b5955203fb7c2d
Kiryjm <kiryjm@gmail.com>
2021-03-17 03:41:47 +0300

    Document, window parameters: client< offset, scroll metrics.

--- js/script.js
+++ js/script.js
@@ -1,52 +1,53 @@
 'use strict';
 
-// Working with dates
+// Document, window parameters
 
-const now1 = new Date('2021-03-15');
-console.log(now1); // 2021-03-15T00:00:00.000Z
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
 
-//output is a Grinvich date considering time zones
-const now2 = new Date(2020, 3, 16, 20);
-console.log(now2); // 2020-04-16T17:00:00.000Z since GMT+3
+      // readonly properties!!!
+const width1 = box.clientWidth; // clientWidth - content+padding
+const height1 = box.clientHeight; //clientHeight - content+padding
+const width2 = box.offsetWidth; // offsetWidth - content+padding+border+margin+scrollbar
+const height2 = box.offsetHeight; //offsetHeight - content+padding+border+margin+scrollbar
+
+// scrollWidth - content+padding+border+margin+scroll part
+const width = box.scrollWidth; 
+//scrollHeight - content+padding+border+margin+scrollbar+scroll
+const height = box.scrollHeight;
+
+console.log(width, height);
+
+// calculate full block height considering scroll
+btn.addEventListener('click', () => {
+    box.style.height = box.scrollHeight + 'px';
+});
 
 
-// Each date in js storing in miliseconds from 1970 - timestamp.
-const now3 = new Date(0);
-console.log(now3); // 1970-01-01T00:00:00.000Z
+// read and write properties!!!
 
+// scrollTop - what is the height of reamining unscrolled content
+btn.addEventListener('mouseover', () => {
+    console.log(box.scrollTop); // amount of scrolled (readed) text
+});
 
-// To use date before 1970 you sould pass negative numbers as argument
-const now4 = new Date(-99999999999);
-console.log(now4); // 1966-10-31T14:13:20.001Z
+// get all element coordinates
+// css: right - distance from right page border to element
+// js: right - distance from left page border to right element border
+// css: bottom - distance from bottom page border to element
+// js: bottom - distance from top page border to bottom element border
 
-// Getting date components
+console.log(box.getBoundingClientRect().top); // 50
 
-const now5 = new Date();
+const style = window.getComputedStyle(box); // get computed styles of elements
+console.log(style.display);
 
-console.log(now5.getFullYear()); // 2021
-console.log(now5.getMonth()); // 2 is March since january is 0!!!!!!
-console.log(now5.getDate()); // 16
-console.log(now5.getDay()); // 2 but Sunday is 0!!!!!!
-console.log(now5.getUTCHours()); // 23 since UTC time zone used
+// get amount of scrolled pixels
+console.log(document.documentElement.scrollTop);
 
-// time difference between local time zone and UTC in minutes!
-console.log(now5.getTimezoneOffset()); // -180
+//Two methods for changing scroll position working with window:
+// scrollBy - scrolls relatively current position
+window.scrollBy(0, 400); // scroll down 400 px from current position
 
-// getTime retuens amount of miliseconds from 1.1.1970
-console.log(now5.getTime()); // 1615852581014
-
-const now6 = new Date();
-console.log(now6.setHours()); // 1615910329088
-console.log(now6); // 2021-03-16T15:59:35.807Z
-
-// calculate time difference (for instance benchmarks 
-// for system, function, loop, operation performance evaluation)
-let start = new Date();
-
-for (let i = 0; i < 100000; i++) {
-    let some = i ** 3; // i^3
-}
-
-let end = new Date();
-
-alert(`Loop iterated for ${end - start} ms`);
+// scrollTo - scrolls relatively all page
+window.scrollTo(0, 400); // scroll down 400 px from top of the page
