059ef23e6a051f408a4e7319a43bf0ee7450cae0
Kiryjm <kiryjm@gmail.com>
2021-04-06 03:29:21 +0300

    Asynchronous server communication using AJAX.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
 'use strict';
 
-// JSON format. Object deep clone
+// Asynchronous tasks and server communication using AJAX.
 
-const persone = {
-    name: 'Alex',
-    tel: '+74444444',
-    parents: {
-        mom: 'Vlana',
-        dad: 'Zhuga'
-    }
-};
+// AJAX - asynchronous javascript and xml
+// technology used to make http requests to server and reload only specified page elements
 
-// stringify() - turns javascript object to JSON format
 
-console.log(JSON.stringify(persone)); // {"name":"Alex","tel":"+74444444"}
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
 
-// parse() - turns JSON format came from server into javascript object
+// input event occurs each time some data was typed or deleted from form
+// change event occurs when element lost its focus 
+inputRub.addEventListener('input', () => {
 
-console.log(JSON.parse(JSON.stringify(persone))); // { name: 'Alex', tel: '+74444444' }
+    // Old object version XMLHttpRequest for working with AJAX
+    const request = new XMLHttpRequest();
 
-//deep clone
-const clone = JSON.parse(JSON.stringify(persone));
-clone.parents.mom = 'Ann';
+    // request type passes in uppercase
+    request.open('GET', 'js/current.json');
 
-console.log(persone); // ... mom: 'Vlana' ...
-console.log(clone); // ... mom: 'Ann' ...
+    // set request header to inform transfer protocols about sending data type and encoding
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    // request sending depending of http request type. If it's POSt we passes argument body
+    request.send();
+
+    // readystatechange event occurs when request state is changed
+    // this event observes readyState property
+    // load event occurs when response is ready
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Something went wrong";
+        }
+    });
+
+    // response status - number
+    // statusText - text after number
+    // response - server response
+    // readyState - number from 0 to 4. Contains current state of our request
+});
