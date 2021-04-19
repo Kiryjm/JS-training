169bac18e4c9804b0b6799bc1647a0c2f8897777
Kiryjm <kiryjm@gmail.com>
2021-04-19 03:56:16 +0300

    Slider dots navigation was added.

--- js/script.js
+++ js/script.js
@@ -336,7 +336,10 @@ window.addEventListener('DOMContentLoaded', () => {
           total = sliderCounter.querySelector('#total'),
           slidesWrapper = slider.querySelector('.offer__slider-wrapper'),
           slidesField = slidesWrapper.querySelector('.offer__slider-inner'),
-          width = window.getComputedStyle(slidesWrapper).width;
+          width = window.getComputedStyle(slidesWrapper).width,
+          indicators = document.createElement('ol'),
+          dots = [];
+
     let slideIndex = 1;
     let offset = 0;
 
@@ -353,6 +356,58 @@ window.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width;
     });
 
+    // Dots styles 
+
+    slider.style.position = 'relative';
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+
+    slider.append(indicators);
+
+    for(let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    function slidesDotsCounterShow() {
+        current.textContent = `${getZero(slideIndex)}`;
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
+
+    // Arrows eventListeners
+
     right.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0;
@@ -368,8 +423,7 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
         
-        current.textContent = `${getZero(slideIndex)}`;
-
+        slidesDotsCounterShow();
     });
 
     left.addEventListener('click', () => {
@@ -387,6 +441,19 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
         
-        current.textContent = `${getZero(slideIndex)}`;
+        slidesDotsCounterShow();
+    });
+
+    // Dots eventListeners
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            slidesDotsCounterShow();
+        });
     });
 });
