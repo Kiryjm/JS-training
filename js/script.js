496634510e679b871bbd16cea7736d48e11dd7f3
Kiryjm <kiryjm@gmail.com>
2021-04-19 05:12:02 +0300

    Storing data in localStorage object.

--- js/script.js
+++ js/script.js
@@ -1,77 +1,59 @@
 'use strict';
 
-// Array iteration methods
+// LocalStorage
 
-// filter() method get callback function with filtering logic 
-// as argument and returns new filtered array
-const names = ['Ivan', 'Ann', 'Kevin','Vasisualiy'];
+// // setting new key-value pair
 
-const shortNames = names.filter(function(name) {
-    return name.length < 5;
+// localStorage.setItem('number', 5);
+
+// // get data from localStorage by key
+
+// console.log(localStorage.getItem('number')); // 5
+
+// // removing data from localStorage
+
+// localStorage.removeItem('number'); // if we remove data before it retrieving, result will be null
+
+// // clear localStorage completely
+
+// localStorage.clear();
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+// if localStorage has key isChecked with value true, make checkbox be checked
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
+
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
+
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
 });
 
-console.log(shortNames); // [ 'Ivan', 'Ann' ]
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
 
-// map() method transform each array element using callback function 
-// passed as argument and returns new array with elements-results of callback function
-const answers = ['IvAn', 'AnnA', 'Hello'];
+// to store objects in localStorage you should convert them to JSON format
+// otherwise we will get value [object Object]
 
-const result = answers.map(item => item.toLowerCase());
-
-console.log(result); // [ 'ivan', 'anna', 'hello' ]
-
-// every()/some() methods check array for condition in callback function 
-// passed as argument and return true, if condition is met on every/at least one 
-// iterable element
-
-const arr = [4, 'qwq', 'steefd'];
-console.log(arr.some(item => typeof(item) === 'number')); 
-// true, cause the first element is number 4
-
-console.log(arr.every(item => typeof(item) === 'number')); // false
-
-// reduce() used to form array from discrete elements
-// result of passed as argument callback function is an argument passing to callback
-// during the next call
-const arrNum = [4, 5, 1, 3, 2, 6];
-
-const res = arrNum.reduce((sum, current) => sum + current);
-console.log(res); // 21
-
-const arr2 = ['apple', 'pear', 'plum'];
-
-const res2 = arr2.reduce((sum, current) => `${sum}, ${current}`);
-console.log(res2); // apple, pear, plum
-
-// reduce() can accept 3rd argument as initial value
-const arr3 = [4, 5, 1, 3, 2, 6];
-
-const res3 = arr3.reduce((sum, current) => sum + current, 3);
-console.log(res3); // 24
-
-const obj = {
-    ivan: 'persone',
-    ann: 'persone',
-    dog: 'animal',
-    cat: 'animal',
+const persone = {
+    name: 'Alex',
+    age: 25
 };
 
-// Object.entries() returns array of object properties and values 
-// in [key, value] format and this order is not guaranteed to match object's one !!!
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('Alex', serializedPersone);
 
-// we can call methods in chain
-// for example to get props names with 'persone' value
-const newArr = Object.entries(obj)
-.filter(item => item[1] === 'persone')
-.map(item => item[0]);
-console.log(newArr); // [ 'ivan', 'ann' ]
-
-// output of Object.entries: array of arrays
-/** 
- * [
-  [ 'ivan', 'persone' ],
-  [ 'ann', 'persone' ],
-  [ 'dog', 'animal' ],
-  [ 'cat', 'animal' ]
-]
-*/
+console.log(JSON.parse(localStorage.getItem('Alex')));
