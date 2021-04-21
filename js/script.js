0bdf90c656eee9c3611b5b24c34433890602b6b0
Kiryjm <kiryjm@gmail.com>
2021-04-21 05:25:48 +0300

    Dividing project into modules.

--- js/script.js
+++ js/script.js
@@ -1,75 +1,31 @@
 'use strict';
 
-// Getters/setters (object properties)
+// Dividing project into modules
 
-// Getter and setter - accessor properties
-// When We call them we don't use brackets as in case of method!
-// When We call setter we use = to assign sum parameter as setter argument!
+const app = '123';
 
-const person = {
-    name: "Alex",
-    age: 27,
+const number = 1;
 
-    get userAge() {
-        return this.age;
-    },
+// anonymous self-called function
+// this function acts like separate module with its own scope
+(function(){
+    let number = 2;
+    console.log(number); // 2
+    console.log(number + 3); // 5
+}());
 
-    set userAge(num) {
-        this.age = num;
-    }
-};
+console.log(number); // 1
 
-console.log(person.userAge); // 27
-console.log(person.userAge = 30); // 30
+// also we can transfer anonymous object with props and methods
+// created by and passed from anonymous self-called function
+const user = (function(){
+    const privat = function() {
+        console.log('I am a private function!');
+    };
 
+    return {
+        sayHello: privat
+    };
+}());
 
-// Incapsulation
-// we can use let variables for object props and getters/setters to make props more private
-// we can use #prop_name syntaxis to make prop private
-
-class User {
-
-    constructor(name, age) {
-        this.name = name;
-        this._age = age;
-    }
-
-    #surname = 'Pavlov';
-    
-    say() {
-        console.log(`User name: ${this.name} ${this.#surname}, age ${this._age}`);
-    }
-
-    get age() {
-        return this._age;
-    }
-
-    set age(age) {
-        if (typeof(age) === 'number' && age > 0 && age < 110) {
-            this._age = age;
-        } else {
-            console.log('Forbidden value!');
-        }
-    }
-
-    get userSurname() {
-        return this.#surname;
-    }
-
-    set userSurname(surname) {
-        this.#surname = surname;
-    }
-}
-
-const ivan = new User('Ivan', 27);
-console.log(ivan._age); // 27
-ivan._age = 99;
-console.log(ivan.age); // 99
-
-ivan.age = 30;
-ivan.age = 300; // Forbidden value!
-console.log(ivan.age); // 30
-console.log(ivan.userSurname); // Pavlov
-ivan.userSurname = 'Drago';
-console.log(ivan.userSurname); // Drago
-ivan.say(); // User name: Ivan Drago, age 27
+user.sayHello(); // I am a private function!
