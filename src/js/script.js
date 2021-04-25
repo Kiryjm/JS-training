9878547726efb20655a762a4c1245193ec249fd1
Kiryjm <kiryjm@gmail.com>
2021-04-25 19:28:21 +0300

    Try/catch/finally. Errors handling

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,67 @@
 'use strict';
 
-// ES6 modules
+// Try/catch/finally. Errors handling
 
-import {one, two} from './main.js';
-import sayDefHi from './main.js';
+// Try/catch wrapped code block stops executing after error in try block
+// and executes instead catch block
+// outer code is still executing despite error in try block
+// try/catch is used to hanlde errors exactly in wrapped by it block code
 
-// we can use alias for imported long name data
-// or use * to import all from module
+try {
+    console.log('Normal');
+    console.log(a);
+    console.log('result');
+} catch(e) {
+    console.log(e);
+}
+console.log('Still normal');  
+// Normal
+// Error
+// Still normal
 
-// To say browser consequentially add script files use type='module' in index.html
-// and use fileName.js syntax in improt statement!
 
-import {one as first} from './main.js';
-import * as data from './main.js';
+// We can handle error object in catch block by properties:
+// name - error type name
+// message - printed message
+// stack - all functions and stack of scripts that made this error happened
+try {
+    console.log('Normal'); // Normal
+    console.log(a);
+    console.log('result');
+} catch(e) {               // Browser console: 
+    console.log(e.name);  // ReferenceError
+    console.log(e.message); // a is not defined
+    console.log(e.stack); // ReferenceError: a is not defined at script.js:28
+}
 
-// // If we use webpack compiled bundle.js in script tag index.html
-// // we use fileName without .js in import statement!
 
-// import {one as first} from './main.js';
-// import * as data from './main.js';
+// finally code block executes in both way any error happened in try block code or not
+// it used to complete started in try code block operations in any case 
 
-console.log(`${first} and ${two}`);
-data.sayHi();
-sayDefHi();
+
+try {
+    console.log('Normal'); // Normal
+    console.log(a);
+    console.log('result');
+} catch(e) {               // Browser console: 
+    console.log(e.name);  // ReferenceError
+    console.log(e.message); // a is not defined
+    console.log(e.stack); // ReferenceError: a is not defined at script.js:28
+} finally {
+    // code executes anyway to complete started operations in any case
+}
+
+
+// To work with multipage sites we can check if page contains required element
+// using try/catch and handling corresponding errors to continue executing 
+// remaining script 
+
+try {
+    document.querySelector('.active').addEventListener('click', () => {
+        console.log('click');
+    });
+} catch (e) {
+    console.log(e);
+}
+
+console.log('normal');
